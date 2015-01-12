56d6edba11970028d95d2ea237ad636b257843f5
Martin Lund <lund@mvno.dk>
2015-01-12 10:33:41 +0100

    Added demo of modelbinding.

---
+++ NancyDemo/ModelBindingModule.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Text;
+using Nancy;
+using Nancy.ModelBinding;
+
+namespace NancyDemo
+{
+	public class ModelBindingModule : NancyModule
+	{
+		public ModelBindingModule()
+		{
+			Get["/modelbinding/"] = _ =>
+			{
+				var helloModel = this.Bind<HelloModel>();
+				return string.Format("Hello {0}, I'm supposed to tell you this: {1}", helloModel.Name, helloModel.Message);
+			};
+
+			Post["/modelbindings/"] = _ =>
+			{
+				var helloModel = this.Bind<List<HelloModel>>();
+
+				var sb = new StringBuilder();
+				helloModel.ForEach(model => sb.Append(string.Format("Hello {0}, I'm supposed to tell you this: {1}", model.Name, model.Message)));
+				return sb.ToString();
+			};
+		}
+	}
+
+	public class HelloModel
+	{
+		public string Name { get; set; }
+		public string Message { get; set; }
+	}
+}
