3d08c0bbeabe2bd3eff62ae0ad54adbf880968d4
Martin Lund <lund@mvno.dk>
2015-01-12 12:07:21 +0100

    Enable diagnostics

---
+++ NancyDemo/DiagnosticsBootstrapper.cs
@@ -0,0 +1,13 @@
+﻿using Nancy;
+using Nancy.Diagnostics;
+
+namespace NancyDemo
+{
+	public class DiagnosticsBootstrapper : DefaultNancyBootstrapper
+	{
+		protected override DiagnosticsConfiguration DiagnosticsConfiguration
+		{
+			get { return new DiagnosticsConfiguration { Password = @"12345" }; }
+		}
+	}
+}
