f0d201ae641441b0650dd0e30241971ea72b8ea0
Martin Lund <lund@mvno.dk>
2015-01-12 12:05:44 +0100

    Added module for demoing contentnegotiation.

---
+++ NancyDemo/ContentNegotiationModule.cs
@@ -0,0 +1,25 @@
+﻿using Nancy;
+
+namespace NancyDemo
+{
+	public class ContentNegotiationModule : NancyModule
+	{
+		public ContentNegotiationModule()
+		{
+			Get["/person/"] = parameters =>
+			{
+				return new Person()
+				{
+					Name = "Martin",
+					Age = 33,
+				};
+			};
+		}
+	}
+
+	public class Person
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+	}
+}
