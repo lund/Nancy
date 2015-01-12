f6289b8c19395572373b565914421c50b59f6cce
Martin Lund <lund@mvno.dk>
2015-01-12 12:17:10 +0100

    Added topshelf host wrapper.

---
+++ NancyDemo/NancyTopshelfHost.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Nancy.Hosting.Self;
+
+namespace NancyDemo
+{
+	public class NancyTopshelfHost
+	{
+		private NancyHost _nancyHost;
+
+		public void Start()
+		{
+			_nancyHost = new NancyHost(new HostConfiguration
+			{
+				UrlReservations = new UrlReservations
+				{
+					CreateAutomatically = true,
+				},
+			},
+			new Uri("http://localhost:8888/"));
+			_nancyHost.Start();
+		}
+
+		public void Stop()
+		{
+			_nancyHost.Stop();
+		}
+	}
+}
