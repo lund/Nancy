dbd7f92b983e74a2af904b1a77b391691c06806a
Martin Lund <lund@mvno.dk>
2015-01-13 08:50:29 +0100

    Added methods for logging the request.

--- NancyDemo/DiagnosticsBootstrapper.cs
+++ NancyDemo/DiagnosticsBootstrapper.cs
@@ -8,6 +8,8 @@ namespace NancyDemo
 {
 	public class DiagnosticsBootstrapper : DefaultNancyBootstrapper
 	{
+		private const string _logKey = "RequestLog";
+
 		protected override DiagnosticsConfiguration DiagnosticsConfiguration
 		{
 			get { return new DiagnosticsConfiguration { Password = @"12345" }; }
@@ -21,6 +23,43 @@ namespace NancyDemo
 				var url = new Uri("http://testesearch200.prod.local:9200");
 				return new ElasticClient(new ConnectionSettings(url).SetDefaultIndex("nancyrequestlogging"));
 			});
+		}
+
+		private Response StartRequestLog(NancyContext context)
+		{
+			var requestLog = new RequestLog(context.Request.Path)
+			{
+				Method = context.Request.Method,
+			};
+
+			context.Items[_logKey] = requestLog;
+			return null;
+		}
+
+		private Response LogEndRequest(TinyIoCContainer container, NancyContext context, Exception exception)
+		{
+			if (context != null && context.Items.ContainsKey(_logKey))
+			{
+				var requestLog = (RequestLog)context.Items[_logKey];
+				requestLog.Stopwatch.Stop();
+				if (exception != null)
+				{
+					requestLog.ResponseCode = 500;
+				}
+				else
+				{
+					requestLog.ResponseCode = (int)context.Response.StatusCode;
+				}
+
+				if (context.NegotiationContext != null)
+				{
+					requestLog.Module = context.NegotiationContext.ModuleName;
+				}
+
+				var analyticsClient = container.Resolve<IElasticClient>();
+				analyticsClient.Index(requestLog);
+			}
+			return null;
 		}
 	}
 }
