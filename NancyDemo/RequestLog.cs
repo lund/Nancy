18c96f6a6a11e017ad80177b6544480b0582897d
Martin Lund <lund@mvno.dk>
2015-01-13 08:46:19 +0100

    Added class for holding request log.

---
+++ NancyDemo/RequestLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace NancyDemo
+{
+	public class RequestLog
+	{
+		public RequestLog(string url)
+		{
+			Stopwatch = new Stopwatch();
+			Stopwatch.Start();
+			Timestamp = DateTime.Now;
+			Id = Guid.NewGuid();
+			Url = url;
+		}
+
+		public Guid Id { get; set; }
+		public string Url { get; set; }
+		public Stopwatch Stopwatch { get; set; }
+		public string Method { get; set; }
+		public DateTime Timestamp { get; set; }
+		public int ResponseCode { get; set; }
+		public string Module { get; set; }
+	}
+}
